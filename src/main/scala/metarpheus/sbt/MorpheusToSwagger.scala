0c294aa44c55e3671cce2b8fb894fa99c98cc96c
samuele reghenzi <sreghenzi@gitlab.eng.it>
2018-01-18 15:16:00 +0100

    missing refs

--- src/main/scala/metarpheus/sbt/MorpheusToSwagger.scala
+++ src/main/scala/metarpheus/sbt/MorpheusToSwagger.scala
@@ -1,13 +1,14 @@
 package metarpheus.sbt
 
 import io.buildo.metarpheus.core.intermediate.Type.{Apply, Name}
-import io.buildo.metarpheus.core.intermediate.{API, RouteParam}
+import io.buildo.metarpheus.core.intermediate.{API, CaseClass, RouteParam}
 import io.swagger.models.parameters.{AbstractSerializableParameter, _}
 import io.swagger.models._
 import io.swagger.models.auth.ApiKeyAuthDefinition
 
 import scala.collection.JavaConverters._
 import io.swagger.models.auth.In
+import io.swagger.models.properties.{AbstractProperty, MapProperty, StringProperty}
 import sbt.internal.util.ManagedLogger
 
 object MorpheusToSwagger {
@@ -15,12 +16,14 @@ object MorpheusToSwagger {
   var log: ManagedLogger = _
 
 
+
+
   def morpheusToSwagger(input: API, name: String, description: String, version: String): Swagger = {
 
 
     val swagger = new Swagger()
     swagger.setBasePath("/api")
-    swagger.setHost("") //TODO PARAMS
+    swagger.setHost("localhost")
     val JSON = "application/json"
     swagger.produces(JSON)
     val schemes = List(Scheme.forValue("https"), Scheme.forValue("http"))
@@ -59,6 +62,20 @@ object MorpheusToSwagger {
         operation.addParameter(buildReqParameter(param))
       })
 
+      operation.setProduces(List("application/json").asJava)
+
+      val response: Response = new Response
+
+      response.setDescription("Response for " + route.name.mkString("_"))
+       val schemaResponse = new AbstractProperty(){}
+
+      val outputName = swaggerType(route.returns).getOrElse("type","string")
+
+      schemaResponse.setType(outputName)
+      response.setSchema(schemaResponse) //  TODO refs
+
+      operation.response(200,response)
+
 
 
       val path = new Path
@@ -66,13 +83,39 @@ object MorpheusToSwagger {
         case "post" => path.setPost(operation)
         case "get" => path.setGet(operation)
       }
-      (route.name.mkString("/"), path)
-    }).groupBy(_._1).map { case (k,v) => (k,v.head._2)}
+      ("/"+route.name.mkString("/"), path)
+    }).toMap
+
+    val definitions : Map[String,Model] = input.models.map{
+      model => {
+        val impl = new ModelImpl()
+        impl.setType("object")
+        model.asInstanceOf[CaseClass].members.foreach( memeber => impl.addProperty(memeber.name,new StringProperty().rename(memeber.name))) //TODO right types
+        (model.name,impl)}
+    }.toMap
 
 
+    swagger.setDefinitions(definitions.asJava)
     swagger.info(info).paths(paths.asJava)
 
 
+  }
+
+  def typeNameToSwaggerType(innerTypeName :String):Map[String,String] = {
+
+    println("typeNameToSwaggerType " +innerTypeName )
+
+    innerTypeName match {
+      case "Int" => Map("type" -> "integer", "format" -> "int32")
+      case "Long" => Map("type" -> "integer", "format" -> "int64")
+      case "Float" => Map("type" -> "number", "format" -> "float")
+      case "Double" => Map("type" -> "number", "format" -> "double")
+      case "Date" => Map("type" -> "string", "format" -> "date")
+      case "DateTime" => Map("type" -> "string", "format" -> "date-time")
+      case "Boolean" => Map("type" -> "boolean")
+      case "List" => Map("type" -> "array")
+      case _ => Map("type" -> "object")
+    }
   }
 
 
@@ -83,6 +126,34 @@ object MorpheusToSwagger {
   private def buildBodyParameter(  param: RouteParam):BodyParameter = {
     buildParameter(param,new BodyParameter).asInstanceOf[BodyParameter]
   }
+
+
+  private def swaggerType( tpe: io.buildo.metarpheus.core.intermediate.Type ):Map[String,String] = {
+
+    val  typeName =tpe match {
+      case n:Name =>  n.name
+      case a:Apply => a.name
+
+    }
+
+
+    typeName match {
+        case "Int" => Map("type" -> "integer", "format" -> "int32")
+        case "Long" => Map("type" -> "integer", "format" -> "int64")
+        case "Float" => Map("type" -> "number", "format" -> "float")
+        case "Double" => Map("type" -> "number", "format" -> "double")
+        case "Date" => Map("type" -> "string", "format" -> "date")
+        case "DateTime" => Map("type" -> "string", "format" -> "date-time")
+        case "Boolean" => Map("type" -> "boolean")
+        case "Option" => typeNameToSwaggerType(tpe.toString)
+        case "List" => Map("type" -> "array")
+        case _ => Map("type" -> "object")
+      }
+
+
+
+  }
+
 
   private def buildParameter(  param: RouteParam , parameter: AbstractParameter):AbstractParameter = {
 
@@ -100,22 +171,12 @@ object MorpheusToSwagger {
       parameter.setRequired(param.required)
 
 
+
     parameter match {
       case sp: AbstractSerializableParameter[_] => {
-        val swaggerParamType = typeName match  {
-          case "Int" => Map("type" -> "integer", "format" -> "int32" )
-          case "Long" => Map("type" -> "integer", "format" -> "int64" )
-          case "Float" => Map("type" -> "number", "format" -> "float" )
-          case "Double" => Map("type" -> "number", "format" -> "double" )
-          case "Date" => Map("type" -> "string", "format" -> "date" )
-          case "DateTime" => Map("type" -> "string", "format" -> "date-time" )
-          case "Boolean" => Map("type" -> "boolean" )
-          case _ => Map("type" -> "object" )
-          //case "Option" => return toSwaggerType(args[0]);
-          //case "List":
-        }
+        val swaggerParamType = swaggerType(param.tpe)
         sp.setType(swaggerParamType.getOrElse("type","object"))
-        sp.setFormat(swaggerParamType.getOrElse("format",""))
+        sp.setFormat(swaggerParamType.getOrElse("format",null))
 
       }
       case _ => println()
